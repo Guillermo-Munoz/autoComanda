8b8f4d7510232bc0a3b93fbec1b33267cc1b5316
pruebagcode <gcode@gcode.com>
2026-01-28 16:28:36 +0100

    Initial commit

---
+++ src/main/java/com/comanda/autocomanda/AutoComandaApplication.java
@@ -0,0 +1,13 @@
+package com.comanda.autocomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoComandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoComandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comanda/autocomanda/AutoComandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comanda.autocomanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoComandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
