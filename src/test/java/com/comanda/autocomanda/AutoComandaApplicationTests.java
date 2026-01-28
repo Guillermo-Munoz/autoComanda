894e8ab478b4b406e2714f0ac24c44188fe96d34
pruebagcode <gcode@gcode.com>
2026-01-28 17:00:34 +0100

    fix formatting and update .gitignore

--- src/test/java/com/comanda/autocomanda/AutoComandaApplicationTests.java
+++ src/test/java/com/comanda/autocomanda/AutoComandaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AutoComandaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
